1c314b25a931797974cfbbf16480a3aaab15dc0b
IreneLi <irene2002yr@gmail.com>
2024-09-22 16:21:54 -0400

    Add email form submission and check for utoronto email.

--- hello.py
+++ hello.py
@@ -6,9 +6,10 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
-# Name submission
-class NameForm(FlaskForm):
+# User Info submission
+class UserForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField("What is your UofT email address?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 
@@ -21,14 +22,33 @@ Moment(app)
 # Handle GET and POST requests (form submission)
 @app.route("/", methods=["GET", "POST"])
 def index():
-    form = NameForm()
+    form = UserForm()
     if form.validate_on_submit():
         old = session.get("name")
+        # Pop up message if the user changes their name
         if old != None and old != form.name.data:
             flash("You have changed your name.")
         session["name"] = form.name.data
+
+        old_email = session.get("email")
+        # Pop up message if the user changes their email
+        if old_email != None and old_email != form.email.data:
+            flash("You have changed your email.")
+
+        # Check for string "utoronto" in input email
+        if "utoronto" not in form.email.data:
+            session["message"] = "Please use your UofT email."
+        else:
+            session["message"] = f"Your UofT email is {form.email.data}"
+        session["email"] = form.email.data
         return redirect(url_for("index"))
-    return render_template("index.html", form=form, name=session.get("name"))
+
+    return render_template(
+        "index.html",
+        form=form,
+        name=session.get("name"),
+        emailMesssage=session.get("message"),
+    )
 
 
 @app.route("/user/<name>")
